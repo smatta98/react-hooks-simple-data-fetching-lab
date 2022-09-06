f05cf7c51b157f7dcc7afff328db3fa6700dee71
Shiv Matta <shiv.matta@gmail.com>
2022-09-06 13:27:11 -0700

    commit tree 9203b79ff863ac0dc943d21b606bab1b912bd6f1

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,36 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+
+function App(){
+    const [dogImage, setDogImage] = useState(null)
+
+useEffect(() => {
+
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((r) => r.json())
+    .then((data) => {
+        setDogImage(data.message)
+    })
+        
+},[])
+
+if (!dogImage){
+    return <p>Loading...</p>
+}
+
+return (
+        <img src = {dogImage} alt = "A Random Dog" />
+)
+
+}
+
+export default App;
+
+
+// This lab is missing the App component that index.js is looking for. To pass the tests in this lab:
+
+// Create an App component from scratch
+// Use the useEffect hook in the App component. Inside the callback for useEffect, 
+// send a fetch request to https://dog.ceo/api/breeds/image/random, a free API that returns a random image of a dog.
+// Display a <p> tag with the text of "Loading..." when the component is first rendered
+// After receiving a response from the API, show the dog image in an <img> tag, with the alt attribute set to "A Random Dog".
